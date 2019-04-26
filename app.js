cdd5b1572aef10585a2a84c8df0e596079a9825a
Louise Iyengar <lki@louiseiyengar.com>
2019-04-26 15:30:47 -0400

    Add error processing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const myName = "Louise Iyengar"
 
 app.use('/static', express.static('public'));
 
@@ -8,7 +9,7 @@ app.set('view engine', 'pug');
 const {projects} = require('./data.json');
 
 app.get('/', (req, res) => {
-    res.render('index', {projects});
+    res.render('index', {projects, myName});
 });
 
 app.get('/about', (req, res) => {
@@ -20,6 +21,18 @@ app.get('/project/:id', (req, res) => {
     id = req.params.id;
     res.locals.project = projects[`${id}`];
     res.render('project');
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Sorry! You requested a page that does not exist.');
+    err.status = 404
+    next(err);
+});
+
+app.use((err, req, res, next) => {  
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {

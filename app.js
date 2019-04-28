84f61f0bdc9cfed7868973416eec19df46b8aa3f
Louise Iyengar <lki@louiseiyengar.com>
2019-04-28 15:59:54 -0400

    Add error processing and projects 4 and 5

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set('view engine', 'pug');
 
 const {projects} = require('./data.json');
 
+const create404Error = () => {
+    const err = new Error('Sorry! You requested a page that does not exist.');
+    err.status = 404
+    return err;
+}
+
 app.get('/', (req, res) => {
     res.render('index', {projects, myName});
 });
@@ -17,24 +23,31 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/project/:id', (req, res) => {
+app.get('/project/:id', (req, res, next) => {
     id = req.params.id;
+    if (isNaN(id) || ((id < 0) || (id > (projects.length - 1)))) {
+        next(create404Error());
+    } 
     res.locals.project = projects[`${id}`];
     res.render('project');
 });
 
 app.use((req, res, next) => {
-    const err = new Error('Sorry! You requested a page that does not exist.');
-    err.status = 404
-    next(err);
+    next(create404Error());
 });
 
 app.use((err, req, res, next) => {  
     res.locals.error = err;
-    res.status(err.status);
+    res.status(err.status || 500);
     res.render('error');
 });
 
 app.listen(3000, () => {
     console.log("The application is running on localhost:3000");
 });
+
+const createError404 = () => {
+    const err = new Error('Sorry! You requested a page that does not exist.');
+    err.status = 404
+    return err;
+}
